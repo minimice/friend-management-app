0aa979d1729415c93f60cf5ad036052fe4b189ba
minimice <skyjuicesoftware@gmail.com>
2018-03-28 22:08:25 +0200

    Remove friendconnectionController

--- app-query/controllers/index.js
+++ app-query/controllers/index.js
@@ -2,13 +2,11 @@
 
 const router = require('express').Router();
 const monitoringController = require('./monitoring');
-const friendconnectionController = require('./friendconnection'); // TO MOVE
 const friendslistController = require('./friendslist');
 const commonfriendslistController = require('./commonfriendslist');
 const receiveupdatesrecipientsController = require('./receiveupdatesrecipientslist');
 
 router.use('/monitoring', monitoringController);
-router.use('/friendconnection', friendconnectionController);
 router.use('/friendslist', friendslistController);
 router.use('/commonfriendslist', commonfriendslistController);
 router.use('/receiveupdatesrecipientslist', receiveupdatesrecipientsController);
