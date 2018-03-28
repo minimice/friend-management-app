9b0e450e317426584832c1dbadde62684f5cb8ae
minimice <skyjuicesoftware@gmail.com>
2018-03-28 23:02:55 +0200

    Add new command controllers

--- app-command/controllers/index.js
+++ app-command/controllers/index.js
@@ -3,8 +3,12 @@
 const router = require('express').Router();
 const monitoringController = require('./monitoring');
 const friendconnectionController = require('./friendconnection');
+const blockupdatesController = require('./blockupdates');
+const subscribeupdatesController = require('./subscribeupdates');
 
 router.use('/monitoring', monitoringController);
 router.use('/friendconnection', friendconnectionController);
+router.use('/blockupdates', blockupdatesController);
+router.use('/subscribeupdates', subscribeupdatesController);
 
 module.exports = router;
