355ce5c3dbe17952dc0f0baebc54257136f4f247
minimice <skyjuicesoftware@gmail.com>
2018-03-28 22:14:11 +0200

    Update port numbers

--- app-command/index.js
+++ app-command/index.js
@@ -11,7 +11,7 @@ app.use(allowAllCors);
 app.use('/api', controllers);
 app.use(errorHandlerMiddleware);
 
-app.listen(8000);
+app.listen(8001);
 
 logger.info("Friend management command API started listening on port 8001");
 logger.info("To test, go to http://localhost:8001/api/monitoring/ping");
