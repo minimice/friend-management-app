3b50234c8750de9273646ee2a4dfa9134d9b38ea
minimice <skyjuicesoftware@gmail.com>
2018-03-28 22:06:37 +0200

    Update Friendconnection command endpoint, will be moved to another folder

--- app-query/controllers/friendconnection.js
+++ app-query/controllers/friendconnection.js
@@ -11,8 +11,12 @@ const db = require('../utilities/db');
 // curl -d '{  "friends": [ "andy@example.com", "john@example.com" ]}' -H 'content-type:application/json' "http://localhost:8000/api/friendconnection"
 // curl -d '{  "friends": [ "andy1@example.com", "john@example.com" ]}' -H 'content-type:application/json' "http://localhost:8000/api/friendconnection"
 // curl -d '{  "friends": [ "andy@example.com", "john2@example.com" ]}' -H 'content-type:application/json' "http://localhost:8000/api/friendconnection"
+// curl -d '{  "friends": [ "andy@example.com", "nonuser@example.com" ]}' -H 'content-type:application/json' "http://localhost:8000/api/friendconnection"
 
 router.post('/', function(req, res) {
+    // Set response header
+    res.setHeader('Content-Type', 'application/json');
+
     if (req && req.body && req.body.friends && req.body.friends.length == 2) {
 
         // Parse the body
@@ -21,52 +25,58 @@ router.post('/', function(req, res) {
         logger.info("User1email: " + user1);
         logger.info("User2email: " + user2);
 
-        // Set response header
-        res.setHeader('Content-Type', 'application/json');
+        logger.info("Friendconnection API POST to connect " + user1 + " and " + user2);
+
+        var user1Id, user2Id;
 
         // Check if they are already connected
         db.executeSql("SELECT * FROM FriendRelationsRead WHERE user1email = ? AND user2email = ?", [user1,user2])
-          .then((result,reject) => {
-            logger.info("Result of friendship: " + result.length);
-            if (result.length == 1) { // already connected!
-                res.send(JSON.stringify({ success: true, message: "Already connected as friends." }, null, 3));
-            } else {
-                reject();
-            }
-          }).catch(() => {
-            // check user1 exists
-            db.executeSql("SELECT * FROM User WHERE email = ?", [user1])
-              .then((result, reject) => {
-                  if (result.length == 0) {
-                      reject();
-                  }
-                  const user1Id = result[0].id;
-                  // check user2 exists
-                  db.executeSql("SELECT * FROM User WHERE email = ?", [user2])
-                    .then((result, reject) => {
-                        if (result.length == 0) {
-                            reject();
-                        }
-                        const user2Id = result[0].id;
-                        // connect them!
-                        db.executeSql('INSERT INTO UserRelationship (user1,user2,status) VALUES ? ON DUPLICATE KEY UPDATE status = 1', [[[user1Id, user2Id, 1]]])
-                          .then((result) => {
-                            db.executeSql('INSERT INTO UserRelationship (user1,user2,status) VALUES ? ON DUPLICATE KEY UPDATE status = 1', [[[user2Id, user1Id, 1]]])
-                              .then((result) => {
-                                res.send(JSON.stringify({ success: true }, null, 3));
-                              });
-                          });
-                    }).catch(() => {
-                        res.send(JSON.stringify({ success: false, message: user2 + " does not exist!" }, null, 3));
-                    });
+            .then((result) => {
+                logger.info("Result of friendship: " + result.length);
+                if (result.length == 1) { // already connected!
+                    res.send(JSON.stringify({ success: true, message: "Already connected as friends." }, null, 3));
+                    return Promise.resolve("success");
+                }
+            }).then((result) => {
+                // resolve if they are already connected
+                if (result && result === "success") {
+                    return Promise.resolve("success");
+                }
+                // check user1 and user2 exists
+                return db.executeSql("SELECT * FROM User WHERE email = ?", [user1])
+                    .then((result) => {
+                        user1Id = result;
+                        return db.executeSql("SELECT * FROM User WHERE email = ?", [user2])
+                    })
+                    .then((result) => {
+                        user2Id = result;
+                    })
+            }).then((result) => {
+                // resolve if they are already connected
+                if (result && result === "success") {
+                    return Promise.resolve("success");
+                }
+
+                if (user1Id.length != 1 && user2Id.length != 1) {
+                    return Promise.reject();
+                }
+                // connect user1 and user2 (if they can be found)
+                const user1ToConnect = user1Id[0].id;
+                const user2ToConnect = user2Id[0].id;
+                return db.executeSql('INSERT INTO UserRelationship (user1,user2,status) VALUES ? ON DUPLICATE KEY UPDATE status = 1', [[[user1ToConnect, user2ToConnect, 1]]])
+                        .then((result) => {
+                            db.executeSql('INSERT INTO UserRelationship (user1,user2,status) VALUES ? ON DUPLICATE KEY UPDATE status = 1', [[[user2ToConnect, user1ToConnect, 1]]])
+                                .then((result) => {
+                                    logger.info("Connected " + user1ToConnect + " and " + user2ToConnect);
+                                    res.send(JSON.stringify({ success: true }, null, 3));
+                                });
+                        });
             }).catch(() => {
-                res.send(JSON.stringify({ success: false, message: user1 + " does not exist!" }, null, 3));
+                res.send(JSON.stringify({ success: false, message: "Unable to connect " + user1 + " and " + user2 }, null, 3));
             });
-          });
-        // otherwise return an error
     } else {
         // Otherwise return an error
-        res.send(JSON.stringify({ success: false }, null, 3));
+        res.send(JSON.stringify({ success: false, message: "JSON request is malformed" }, null, 3));
     }
 });
 
--- app-query/controllers/index.js
+++ app-query/controllers/index.js
@@ -2,9 +2,15 @@
 
 const router = require('express').Router();
 const monitoringController = require('./monitoring');
-const friendconnectionController = require('./friendconnection');
+const friendconnectionController = require('./friendconnection'); // TO MOVE
+const friendslistController = require('./friendslist');
+const commonfriendslistController = require('./commonfriendslist');
+const receiveupdatesrecipientsController = require('./receiveupdatesrecipientslist');
 
 router.use('/monitoring', monitoringController);
 router.use('/friendconnection', friendconnectionController);
+router.use('/friendslist', friendslistController);
+router.use('/commonfriendslist', commonfriendslistController);
+router.use('/receiveupdatesrecipientslist', receiveupdatesrecipientsController);
 
 module.exports = router;
