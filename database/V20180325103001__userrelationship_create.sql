78878a702944afaee0f77560dc453b50b5046e82
minimice <skyjuicesoftware@gmail.com>
2018-04-02 11:53:45 +0200

    Add comment describing how the relationship between users works

--- database/V20180325103001__userrelationship_create.sql
+++ database/V20180325103001__userrelationship_create.sql
@@ -12,3 +12,9 @@ CREATE TABLE friendshipdb.UserRelationship (
 -- 3 means blocked (user1 blocked user2)
 -- 4 means unblocked (user1 unblocked user2)
 -- 5 means unfriended (user1 unfriended user2)
+
+-- User1 and user2 are only "friends" when the status is 1 in both directions
+-- I am treating this as a directed graph, that is A -> B and B <- A.
+-- Both vertices ("status") must have a value of 1 in order for the friendship to be valid.
+-- If both status values are 1, then this friendship will automatically be shown in the FriendRelationsRead view
+-- That means if User1 blocks User2 or vice versa (i.e. status has a value of 3), this friendship is no longer valid.
